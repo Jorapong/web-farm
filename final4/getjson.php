2cb09a9abd5ec6c14100bc6488fa0d546a80315e
Jorapong <37506852+Jorapong@users.noreply.github.com>
2021-02-07 14:40:23 +0700

    new

---
+++ final4/getjson.php
@@ -0,0 +1,15 @@
+<?php
+	header('Content-Type: application/json');
+	$conn = mysqli_connect("localhost","root","","test");
+
+	$sql = "SELECT * FROM sensor_value where sensorv_id =".$_GET['sensorv_id'];
+
+	$query = mysqli_query($conn,$sql);
+	$resultArray = array();
+	while($result = mysqli_fetch_array($query,MYSQLI_ASSOC))
+	{
+		array_push($resultArray,$result);
+	}
+	mysqli_close($conn);
+	echo json_encode($resultArray);
+?>
